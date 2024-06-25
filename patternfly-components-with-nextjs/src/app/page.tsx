a5d6b361652da5e4af31c482ecd43a6588b515db
Christoph Jerolimov <jerolimov+git@gmail.com>
2024-06-25 20:32:38 +0200

    PatternFly component demo

--- patternfly-components-with-nextjs/src/app/page.tsx
+++ patternfly-components-with-nextjs/src/app/page.tsx
@@ -1,95 +1,54 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { Page, PageSection, PageSectionVariants, TextContent, Text, Button, AlertGroup, Alert, AlertVariant, AlertProps, AlertActionCloseButton } from "@patternfly/react-core";
+
+import '@patternfly/react-core/dist/styles/base.css';
+import { useState } from "react";
 
 export default function Home() {
+  const [alerts, setAlerts] = useState<AlertProps[]>([]);
+
+  const addAlert = () => {
+    const newAlert: AlertProps = {
+      key: new Date().getTime(),
+      title: 'Hello!',
+      variant: AlertVariant.info,
+    };
+    setAlerts((prevAlerts) => [...prevAlerts, newAlert]);
+  };
+
+  const removeAlert = (key: React.Key) => {
+    setAlerts((prevAlerts) => [...prevAlerts.filter((alert) => alert.key !== key)]);
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <Page
+        additionalGroupedContent={
+          <PageSection variant={PageSectionVariants.light} isWidthLimited>
+            <TextContent>
+              <Text component="h1">Main title</Text>
+              <Text component="p">This is a full page demo.</Text>
+            </TextContent>
+          </PageSection>
+        }
+      >
+        <PageSection>
+          <Button variant="primary" onClick={addAlert}>Hello, world!</Button>
+        </PageSection>
+      </Page>
+      <AlertGroup isToast isLiveRegion>
+        {alerts.map(({ key, ...otherProps }) => (
+          <Alert
+            key={key}
+            {...otherProps}
+            actionClose={
+              <AlertActionCloseButton onClose={() => removeAlert(key!)} />
+            }
+          />
+        ))}
+      </AlertGroup>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
