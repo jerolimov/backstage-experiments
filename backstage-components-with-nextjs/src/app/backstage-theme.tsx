6a343864987d16288949eebfcbd93358cfa3ecc1
Christoph Jerolimov <jerolimov+git@gmail.com>
2024-06-25 15:07:27 +0200

    Use RHDH theme for the backstage component demo

--- backstage-components-with-nextjs/src/app/backstage-theme.tsx
+++ backstage-components-with-nextjs/src/app/backstage-theme.tsx
@@ -1,12 +1,26 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 "use client";
-import { UnifiedThemeProvider, themes } from '@backstage/theme';
+import { UnifiedThemeProvider, themes as backstageThemes } from '@backstage/theme';
+import { useThemes } from '@redhat-developer/red-hat-developer-hub-theme';
+
+const useBackstageTheme = false;
 
 export function BackstageThemeProvider(props: Readonly<{
   children: React.ReactNode;
 }>) {
+  if (useBackstageTheme) {
+    return (
+      <UnifiedThemeProvider theme={backstageThemes.light}>
+        {props.children}
+      </UnifiedThemeProvider>
+    );
+  }
+
+  const rhdhThemes = useThemes();
+  const RHDHThemeProvider = rhdhThemes[0].Provider;
   return (
-    <UnifiedThemeProvider theme={themes.light}>
+    <RHDHThemeProvider>
       {props.children}
-    </UnifiedThemeProvider>
+    </RHDHThemeProvider>
   );
 }
